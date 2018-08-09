65f36816711e97e302e922fe1c7ea8cdca149bc8
Supersmf <Supersmf@mail.ru>
2018-08-10 00:25:45 +0300

    bookworm-react/ feature: add fields in form

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -3,14 +3,45 @@ import { Form, Button } from 'semantic-ui-react';
 
 class LoginFormg extends Component {
 	state = {
-		data: {},
+		data: {
+			email: '',
+			password: ''
+		},
 		loading: false,
 		errors: {}
 	}
+	onChange = e =>
+		this.setState({
+			data: {...this.state.data, [e.target.name]: e.target.value}
+		});
 
     render() {
+    	const { data } = this.state;
+
         return (
             <Form>
+            	<Form.Field>
+            		<label htmlFor="email">Email</label>
+            		<input
+            			type="email"
+            			id="email"
+            			name="email"
+            			placeholder="example@example.com"
+            			value={data.email}
+            			onChange={this.onChange}
+            		/>
+            	</Form.Field>
+            	<Form.Field>
+            		<label htmlFor="password">Password</label>
+            		<input
+            			type="password"
+            			id="password"
+            			name="password"
+            			placeholder="Make it secure"
+            			value={data.password}
+            			onChange={this.onChange}
+            		/>
+            	</Form.Field>
             	<Button primary>Login</Button>
             </Form>
         );
